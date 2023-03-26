74b0ada6aeaa100b27a59d2cd3800f09a7dc7e80
BestIsReaLoL <71178802+BestIsReaLoL@users.noreply.github.com>
2023-03-26 23:34:23 +0300

    Update control.js

--- control.js
+++ control.js
@@ -2,28 +2,28 @@ const Discord = require('discord.js');
 const { SerialPort } = require('serialport');
 const client = new Discord.Client();
 
-const port = new SerialPort({path: "COM3",  baudRate: 9600 });
+const port = new SerialPort({path: "COM3",  baudRate: 9600 }); //arduino kartının takılı olduğu port "COM3"
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`${client.user.tag} > Discord api ile bağlantı kuruldu`);
 });
 
 client.on('message', message => {
   if (message.content === '!led aç') {
-    port.write('1');
+    port.write('led_1');
     message.reply("Led açık!")
   } else if (message.content === '!led kapat') {
-    port.write('0');
+    port.write('led_0');
     message.reply("Led kapalı!")
-  } else if (message.content === '!buzzer aç') {
+  } /* else if (message.content === '!buzzer aç') {
     port.write('buzzer_1');
     message.reply("Buzzer açık!")
   } else if (message.content === '!buzzer kapat') {
     port.write('buzzer_0');
     message.reply("Buzzer kapalı!")
-  }
+  } */
 });
 
 
 
-client.login('MTA3MDEwMDk4MTAwNzc5NDI4OQ.GRLpLq.VZ_n6rXOW15n3DB7TuUOBdFRS3OilujoAryiAE');
+client.login('token');
